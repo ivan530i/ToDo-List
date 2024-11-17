6cb48f71f1ee5da6886b7c8ec56980a2167e53fb
ivan530i <pchelnikov79@gmail.com>
2024-11-17 14:39:51 +0300

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Pchelnikov Ivan on 17.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDo ListTests/ToDo_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListTests.swift
+//  ToDo ListTests
+//
+//  Created by Pchelnikov Ivan on 17.11.2024.
+//
+
+import Testing
+@testable import ToDo_List
+
+struct ToDo_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
